ba54ea490a7a009344450ec6008bd8ec723b4519
Lee <653498671@qq.com>
2018-01-18 17:51:46 +0800

    初始化文件

---
+++ app/index.js
@@ -0,0 +1,14 @@
+// function component() {
+//     var elememt = document.createElement('div');
+//     elememt.innerHTML = "hello ,welcome to react-webpack";
+//     return elememt;
+// }
+// document.body.appendChild(component());
+
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+ReactDOM.render(
+    <div>hello lee</div>,
+    document.getElementById('app')
+)
---
+++ webpack.config.js
@@ -0,0 +1,30 @@
+var path = require('path');
+
+module.exports = {
+    //入口文件
+    entry: './app/index',
+    //出口文件
+    output:{
+        filename:'index.js',
+        path:path.resolve(__dirname,'dist'),
+        publicPath:'temp/'
+    },
+    devServer:{
+        contentBase:'./',
+        host:'localhost',
+        compress:true,
+        port:1717
+    },
+    module:{
+        loaders:[
+            {
+                test:/\.js$/,
+                exclude:/node_modules/,
+                loaders:"babel-loader",
+                query:{
+                    presets:['es2015','react']
+                }
+            }
+        ]
+    }
+}
